b70132e9191b888cf2529761a4bacb5b22319a11
AndrewBall79 <andy.bob.ball@gmail.com>
2021-01-22 21:45:24 -0600

    onclick

--- Ex_Files_JavaScript_EssT/Exercise Files/08_03/script.js
+++ Ex_Files_JavaScript_EssT/Exercise Files/08_03/script.js
@@ -5,13 +5,20 @@ const resetButton = document.querySelector("#reset");
 const theTimer = document.querySelector(".timer");
 
 var timer = [0,0,0,0];
+var interval;
+var timerRunning = false;
 
 // Add leading zero to numbers 9 or below (purely for aesthetics):
-
+function leadingZero(time) {
+    if (time <= 9){
+    time = "0" + time;
+    }
+    return time;
+}
 
 // Run a standard minute/second/hundredths timer:
 function runTimer() {
-    let currentTime = timer[0] + ":" + timer[1] + ":" + timer[2]
+    let currentTime = leadingZero(timer[0]) + ":" + leadingZero(timer[1]) + ":" + leadingZero(timer[2]);
     theTimer.innerHTML = currentTime;
     timer[3]++;
 
@@ -22,15 +29,27 @@ function runTimer() {
 
 // Match the text entered with the provided text on the page:
 function spellCheck() {
-    let textEnteredLength = testArea.value;
-    console.log(textEnteredLength);
+    let textEntered = testArea.value;
+    let originTextMatch = originText.substring(0, textEntered.length)
+
+    if (textEntered == originText){
+        clearInterval(interval);
+        testWrapper.style.borderColor = "#429890";
+    }else{
+        if (textEntered == originTextMatch){
+            testWrapper.style.borderColor = "#65ccf3";
+        }else {
+            testWrapper.style.borderColor = "#E95D0F";
+        }
+    }
 }
 
 // Start the timer:
 function start() {
     let textEnteredLength = testArea.value.length;
-    if (textEnteredLength === 0 ){
-        setInterval(runTimer, 10);
+    if (textEnteredLength === 0 && !timerRunning){
+        timerRunning = true;
+        interval = setInterval(runTimer, 10);
 
     }
     console.log(textEnteredLength);
@@ -38,10 +57,17 @@ function start() {
 
 // Reset everything:
 function reset() {
-    console.log("reset pressed");
+    clearInterval(interval);
+    interval = null;
+    timer = [0,0,0,0];
+    timerRunning = false;
+
+    testArea.value = "";
+    theTimer.innerHTML = "00:00:00";
+    testWrapper.style.borderColor = "grey"
 }
 
 // Event listeners for keyboard input and the reset button:
 testArea.addEventListener("keypress", start, false);
 testArea.addEventListener("keyup", spellCheck, false);
-resetButton.addEventListener("keypress", start, false);
+resetButton.addEventListener("click", reset, false);
